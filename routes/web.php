a64fc8c070fd43af1da4ceecf38c887edf7e07ad
Jeremy <the.jeremy.cobb@gmail.com>
2024-12-01 15:08:18 -0600

    add route for seeking-synths test page

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('seeking-synths');
 });
+
+Route::get('/test', function () {
+    return view('seeking-synths-test');
+});
